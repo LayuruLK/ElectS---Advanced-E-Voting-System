857d5f8e2bcbac1c2ec47a7563ac6a9d42ceb2fa
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-27 00:45:00 +0530

    Merge Login Component

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Login</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

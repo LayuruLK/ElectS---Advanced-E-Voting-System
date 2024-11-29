ad766f0346f2ec59f1fd2db048e759e10bb38c17
Thasleem-Basheery <154669750+BasheeryMKT@users.noreply.github.com>
2024-11-30 00:43:15 +0530

    Backend Updated

---
+++ Backend/routers/admin.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { Admin } = require('../models/admin');
+const { User } = require('../models/user');
+const { Candidate } = require('../models/candidate');
+const { PoliticalParty } = require('../models/party');
+const Service = require('../Services/GenericService');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const name = 'Admin';
+
+//Get All Users
+router.get('/users', async(req,res)=>{
+    try{
+        const users = await Service.getAll(res,User,'User');
+        res.status(200).json(users);
+       
+    }
+    catch(error){
+        res.status(500).send(error + 'Server Error');
+    }
+});
+
+module.exports = router;
+

ef7f167353f2d51f33cf711b35acdff9fd2e3f51
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-12-04 19:39:54 +0530

    Connected loginsignup page to App.js

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-//import LoginSignup from './Pages/LoginSignup';
+import LoginSignup from './Pages/LoginSignup';
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
           <Navbar/>
 
           <Routes>
-            {/* <Route path='/login' element={<LoginSignup/>}/> */}
+            <Route path='/login' element={<LoginSignup/>}/>
           </Routes>
       </BrowserRouter>
      

d5379232be1315b2748b3571141e8d006ad9b5cb
Fathima Minha <minhasulaikar@gmail.com>
2024-12-10 22:22:44 +0530

    create route for add election to the election model

--- Backend/routers/Elections.js
+++ Backend/routers/Elections.js
@@ -30,4 +30,39 @@ router.get('/get/count', (req, res) => {
     });
 });
 
+router.post('/', async (req, res) => {
+    const { name, where, date, startTime, endTime, description, rules } = req.body;
+  
+    if (!name || !where || !date || !startTime || !endTime || !description) {
+      return res.status(400).json({ success: false, message: "Please fill all the required fields!" });
+    } 
+    try {
+      const electionDate = new Date(date);  
+      const electionStartTime = new Date(startTime);  
+      const electionEndTime = new Date(endTime);  
+      electionStartTime.setFullYear(electionDate.getFullYear(), electionDate.getMonth(), electionDate.getDate());
+      electionEndTime.setFullYear(electionDate.getFullYear(), electionDate.getMonth(), electionDate.getDate());
+  
+      let election = new Election({
+        name,
+        where,
+        date: electionDate,
+        startTime: electionStartTime,
+        endTime: electionEndTime,
+        description,
+        rules
+      });
+  
+      election = await election.save();
+  
+      if (!election) {
+        return res.status(400).json({ success: false, message: "Election could not be added!" });
+      }
+  
+      res.status(201).json({ success: true, message: "Successfully added", data: election });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 module.exports=router;

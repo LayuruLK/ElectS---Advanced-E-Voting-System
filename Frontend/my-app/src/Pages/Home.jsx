513a4c7bc0c666c07e10ccc000d13b7fc46b7837
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-12-13 00:04:14 +0530

    feat: modify landing page by adding new components

--- Frontend/my-app/src/Pages/Home.jsx
+++ Frontend/my-app/src/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Welcome from '../Components/Welcome/Welcome'
+import FandQ from '../Components/Fandq/Fandq'
+import Features from '../Components/Feature/Feature'
+import Feature from '../Components/Feature/Feature'
 
 const Home = () => {
   return (
     <div>
       <Welcome/>
+      <Feature/>
+      <FandQ/>
     </div>
   )
 }

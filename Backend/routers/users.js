14a493462bba28fee76fc17351bf85f756a8f7dd
Thasleem-Basheery <154669750+BasheeryMKT@users.noreply.github.com>
2024-12-13 14:22:57 +0530

    User Router Updated

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -227,4 +227,25 @@ router.get('/pending-verifications', async (req,res)=> {
     }
 });
 
+//API Route to Verify or Reject Users
+router.put('/verify/:userId', async (req,res) => {
+    const { isVerified } = req.body;
+
+    try {
+        const user = await User.findById(req.params.userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User Not Found'});
+        }
+
+        user.isVerified = isVerified;
+        await user.save();
+
+        res.status(200).json({ success: true, message:`User ${isVerified ? 'approved' : 'rejected'} successfully` });
+    } catch (error) {
+        console.error('Error updating verification status:', error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error'});
+    }
+})
+
 module.exports = router;

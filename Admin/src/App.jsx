1eef1ececb1d89eb22a35463a3d489cf8a3f1315
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-12-18 15:00:54 +0530

    feat:

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Review from "./components/Review/Review";
@@ -9,6 +9,7 @@ import CandidateReview from "./components/CandidateReview/CandidateReview";
 import CandidateProfile from "./components/CandidateProfile/CandidateProfile";
 import Party from './components/Party/Party';
 import AddParty from './components/AddParty/AddParty';
+import AdminRegister from './components/Register/Register';
 
 function App() {
   return (
@@ -18,14 +19,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/> 
         <Route path="/login" element={<Login/>}/>
-
         <Route path='/review' element={<Review/>}/>
         <Route path='/nic-review' element={<NICReview/>}/>
         <Route path='/candidate-review' element={<CandidateReview/>}/>
         <Route path='/candidate-profile/:id' element={<CandidateProfile/>}/>
-
         <Route path='/party' element={<Party/>}/>
         <Route path='/add-party' element={<AddParty/>}/>
+        <Route path="/register" element={<AdminRegister />} />
       </Routes>
      </BrowserRouter>
     

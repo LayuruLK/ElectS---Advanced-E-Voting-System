21aa4381cba2d6812aac004a8379fdb6b2ab8638
kushipabo <139679251+kushipabo@users.noreply.github.com>
2024-12-22 21:43:33 +0530

    feat:Add basic structure for the project submission form.

--- Frontend/my-app/src/Components/AddProjectCandidate/AddProjectCandidate.jsx
+++ Frontend/my-app/src/Components/AddProjectCandidate/AddProjectCandidate.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import './AddProjectCandidate.css';
 
 const AddProjectCandidate = () => {
     const checkIfUserIsCandidate = async () => {
@@ -11,6 +12,27 @@ const AddProjectCandidate = () => {
           return false;
         }
       };
+      return (
+        <div className="add-project-container">
+            <form>
+                <h2>Add new Project</h2>
+                <div className="form-group">
+                    <label>Title</label>
+                </div>
+                <div className="form-group">
+                    <label>Description</label>
+                </div>
+                <div className="form-group">
+                    <label>Links</label>
+                </div>
+                <div className="form-group">
+                    <label>Attachments</label>
+                </div>
+                <button type="submit">Add Project</button>
+            </form>
+        </div>
+    );
 };
+
 
 export default AddProjectCandidate;

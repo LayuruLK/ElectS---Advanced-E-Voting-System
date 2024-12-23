b3b26373ff4a148f94e33c13c19f7c84c8d19566
kushipabo <139679251+kushipabo@users.noreply.github.com>
2024-12-23 11:54:49 +0530

    feat:"Add usestate for project submission form inputs"

--- Frontend/my-app/src/Components/AddProjectCandidate/AddProjectCandidate.jsx
+++ Frontend/my-app/src/Components/AddProjectCandidate/AddProjectCandidate.jsx
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import './AddProjectCandidate.css';
+import React, { useState } from 'react';
 
 const AddProjectCandidate = () => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [links, setLinks] = useState('');
+
     const checkIfUserIsCandidate = async () => {
         try {
           const response = await axios.get(`http://localhost:5000/api/v1/users/profile/${userId}`);
@@ -18,12 +24,28 @@ const AddProjectCandidate = () => {
                 <h2>Add new Project</h2>
                 <div className="form-group">
                     <label>Title</label>
+                    <input 
+                        type="text" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required 
+                    />
                 </div>
                 <div className="form-group">
                     <label>Description</label>
+                    <textarea 
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required 
+                    ></textarea>
                 </div>
                 <div className="form-group">
                     <label>Links</label>
+                    <input 
+                        type="url" 
+                        value={links}
+                        onChange={(e) => setLinks(e.target.value)}
+                    />
                 </div>
                 <div className="form-group">
                     <label>Attachments</label>

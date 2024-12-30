8490f83ccff553ec8e399e7b8b1780e7fa3877c7
Thasleem-Basheery <154669750+BasheeryMKT@users.noreply.github.com>
2024-12-30 22:39:19 +0530

    Frontend Candidate Profile

---
+++ Frontend/my-app/src/Components/CandidateProfile/CandidateProfile.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import './CandidateProfile.css';
+import Complaint from '../Complaint/Complaint';
+import { FaUser, FaTasks, FaExclamationCircle } from 'react-icons/fa'; // React Icons
+
+
+
+
+
+
+
+
+
+
+export default CandidateProfile;

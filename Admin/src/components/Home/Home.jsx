c786eaa74f0cc756426853b3993813daa019294b
Kasun Rajapaksha <kasunrajapaksha2001@gmail.com>
2025-01-01 21:15:06 +0530

    Admin Home updated

--- Admin/src/components/Home/Home.jsx
+++ Admin/src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './Home.css';
+import React from 'react'; // Assuming the sidebar component is in the same directory
+import './Home.css'; // Import the CSS file
+import HomeSideBar from '../HomeSideBar/HomeSideBar';
+import Projects from '../Projects/Projects';
 
 const Home = () => {
   return (
-    <div className='hme-div'>
-      This is a home page
+    <div className="home-container">
+      <div className="mainn-content">
+        <h3>This is the Home Page</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

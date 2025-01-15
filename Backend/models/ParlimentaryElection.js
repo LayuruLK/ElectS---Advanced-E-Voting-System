4abc092b8e69ca5eaab7174504bec8f0dfdf127b
Thasleem-Basheery <154669750+BasheeryMKT@users.noreply.github.com>
2025-01-16 00:19:27 +0530

    ParlimentaryElection Model Updated

--- Backend/models/ParlimentaryElection.js
+++ Backend/models/ParlimentaryElection.js
@@ -28,13 +28,38 @@ const ParlimentaryElectionSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Candidate'
     }],
-
-
-
-
-
-
-
+    results: {
+        totalVotes: { type: Number, default: 0 }, // Total votes cast
+        winningCandidate: {
+            candidateId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Candidate'
+            },
+            name: { type: String } // Candidate's name for quick access
+        },
+        winningParty: {
+            partyId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'PoliticalParty'
+            },
+            name: { type: String } // Party name for quick access
+        },
+        voteDistribution: [{
+            candidateId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Candidate'
+            },
+            votes: { type: Number, default: 0 }, // Votes received by the candidate
+            voters: [{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }]
+        }]
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    }
 });
 
 const ParlimentaryElection = mongoose.model('ParliamentaryElection', ParlimentaryElectionSchema);

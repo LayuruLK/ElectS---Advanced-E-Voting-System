ee82ce2b0f6312e97348b54615d1912d47c86b99
Bingusala <chinadaribingusala000@gmail.com>
2025-01-20 00:28:22 +0530

    Add initial structure for PresidentialElection component

---
+++ Admin/src/components/Elections/PresidentialElection/PresidentialElection.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './PresidentialElection.css'
+
+const PresidentialElection = () => {
+  return (
+    <div className='presidential-election-container'>
+      <h2>Presidential Election Form</h2>
+      <form className='presidential-election-form'></form>
+    </div>
+  )
+};
+
+export default PresidentialElection;

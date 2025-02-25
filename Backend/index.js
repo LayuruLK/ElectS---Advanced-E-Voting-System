3e3b9b46b4f0766fa702b9403e245f4f7faab31a
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-02-25 19:22:48 +0530

    feat:add fake report routes

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,8 @@ const parlimentaryElectionsRoutes = require('./routers/parlimentaryElections');
 const provinvialElectionsRoutes = require('./routers/provincialElections');
 const passwordRecoveryRoutes = require('./routers/passwordrecoveryroute');
 const verificationsRoutes = require('./routers/verifications');
-const candidateDescriptionRoutes = require('./routers/candidateDescriptionRoutes')
+const candidateDescriptionRoutes = require('./routers/candidateDescriptionRoutes');
+const reportFakesRoutes = require('./routers/reportFakes');
 
 
 const api = process.env.API_URL
@@ -50,6 +51,7 @@ app.use(`${api}/passwords`, passwordRecoveryRoutes);
 app.use(`${api}/passwords`, passwordRecoveryRoutes);
 app.use(`${api}/verifications`, verificationsRoutes);
 app.use(`${api}/description`, candidateDescriptionRoutes);
+app.use(`${api}/reportFakes`, reportFakesRoutes);
 
 // Check for required environment variables
 if (!process.env.CONNECTION_STRING || !process.env.PORT) {
